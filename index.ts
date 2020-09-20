82792d1c6d3fe90ccc1645f031fa455b3694d884
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2020-09-20 22:46:22 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,40 @@
+import Nightmare from 'nightmare';
+require('nightmare-download-manager')(Nightmare);
+async function main() {
+    let nightmare = new Nightmare({ show: true });
+    ///@ts-ignore
+    nightmare.on('download', (state, downloadItem) => {
+        if (state == 'started') {
+            // debugger;
+            ///@ts-ignore
+            nightmare.emit('download', './file.zip', downloadItem);
+        }
+    });
+    nightmare
+        ///@ts-ignore
+        .downloadManager();
+    nightmare.goto('https://ultrastar-es.org/foro/ucp.php?mode=login')
+        .type('#username', 'Luigi003')
+        .type('#password', '')
+        .type('#password', '\u000d')
+        .wait('#username_logged_in')
+        .goto('https://ultrastar-es.org/es/canciones?busqueda=phineas+y+ferb')
+        .click('.canciones > li > .acciones > li:nth-child(3) > button')
+        .click('.canciones > li:nth-child(2) > .acciones > li:nth-child(3) > button')
+        .click('#descargar_paquete')
+        // .wait(10000)
+        // .goto('https://ultrastar-es.org/es/canciones/descargar/paquete')
+        ///@ts-ignore
+        .waitDownloadsComplete()
+    nightmare.evaluate(() => {
+        const thingy = document.querySelector('.canciones > li > h3 > a');
+        return thingy?.textContent;
+    })
+        .end()
+        .then(console.log)
+        .catch((error: any) => {
+            console.error('Error:', error)
+        })
+}
+
+main().then(() => { });
