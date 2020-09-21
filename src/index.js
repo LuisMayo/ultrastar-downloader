789884e94ffd7b2fecbd1a49ebbccf5f64e27c0f
LuisMayo <luismayovalbuena@outlook.es>
2020-09-21 15:14:01 +0000

    Changed google cloud function to express

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.download = (req, res) => {
-  const {main} = require('./ultrastar');
-  main().then((val) => {
-      res.status(200).send('DONE! ' + val);
-  }).catch((e) => {
-      res.status(500).send('Fail! ' + e);
-  });
-};
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    const { main } = require('./ultrastar');
+    main().then((val) => {
+        res.status(200).send('DONE! ' + val);
+    }).catch((e) => {
+        res.status(500).send('Fail! ' + e);
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
