6dc3c3488d4921940b392a99c934027fe4095852
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2020-09-29 23:58:39 +0200

    Changed script to be a locally-executed script

--- src/index.js
+++ src/index.js
@@ -1,27 +1,4 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-const express = require('express')
-const cors = require('cors')
-const app = express()
-
-app.use(express.json(), cors())
-const port = process.env.PORT || 8080
-
-app.post('/', (req, res) => {
-    const { main } = require('./ultrastar');
-    main(req.body).then((val) => {
-        res.download(__dirname + '/' + req.body.username + '/songs.zip');
-    }).catch((e) => {
-        res.status(500).send('Fail! ' + e);
-    });
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
-
+const fs = require('fs');
+const input = JSON.parse(fs.readFileSync(process.argv[2]));
+const ultrastar = require('./ultrastar');
+ultrastar.main(input).then(() => {});
