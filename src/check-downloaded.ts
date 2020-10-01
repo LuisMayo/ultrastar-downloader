51216edeba4d57f1e4824358df1442f40000f53e
LuisMayo <luismayovalbuena@outlook.es>
2020-10-01 17:06:38 +0200

    Check downloaded songs before trying to download them again

--- src/check-downloaded.ts
+++ src/check-downloaded.ts
@@ -1,15 +1,26 @@
 import { Petition } from "./petition";
 import fs from 'fs';
 import { compareTwoStrings } from 'string-similarity';
+import { Utils } from "./utils";
 
 /* Check what songs weren't foind
 Q: Isn't there a better way to do this?
 A: I'd love to, but nightmare doesn't seem to be very friendly when we're trying to extract data from
 the evaluate function. So I can't really know whether a song passed the check or not.
 If a way is found, this whole file can be deleted and I'll be more than happy to do it  */
-export function checkDownloaded(req: Petition) {
+/**
+ * 
+ * @param req Petition
+ * @param internal Do we need the list for programatic reasons (true) or do we want to output to user (false)
+ */
+export function checkDownloaded(req: Petition, internal = false) {
     // We get all the downloaded songs
-    const dir = fs.readdirSync(__dirname + '/' + req.username + '/songs/');
+    let dir: string[];
+    try {
+        dir = fs.readdirSync(__dirname + '/' + req.username + '/songs/');
+    } catch (e) {
+        dir = [];
+    }
     const songStrings = dir.map(fileName => fileName.substring(fileName.lastIndexOf('#') + 1, fileName.indexOf('.torrent')).trim());
     // We map it to an object similar to Petition
     const songs = songStrings.map(string => {
@@ -18,6 +29,8 @@ export function checkDownloaded(req: Petition) {
             title: string.substring(string.indexOf(' - ') + 3)
         }
     });
+    songs.forEach(Utils.normalizeSong);
+    req.songs.forEach(Utils.normalizeSong);
     // We only want the non-dowloaded songs
     const rejectedSongs = req.songs.filter(
         // To check if a song is downloaded or not we search it on the songs we obtained from reading the downloaded songs filename
@@ -26,6 +39,11 @@ export function checkDownloaded(req: Petition) {
             && compareTwoStrings(petitionSong.title.toLocaleLowerCase(), downloadedSong.title.toLocaleLowerCase()) > 0.75
         ) == null // If we didn't found a match (i.e: find result is null) it means we don't have that song so we return true to the filter function so this gets included in the array
     );
+
+    if (internal) {
+        req.songs = rejectedSongs;
+        return;
+    }
 
     console.log(`We couldn't find ${rejectedSongs.length} song(s)`);
     // Get input fileName
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 const input = JSON.parse(fs.readFileSync(process.argv[2]));
 const ultrastar = require('./ultrastar');
 const { checkDownloaded } = require('./check-downloaded');
+checkDownloaded(req, true);
 ultrastar.main(input).then(() => {checkDownloaded(input);});
